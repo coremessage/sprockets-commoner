2ccf122e237c09d998b58b357260a1543519028a
coremessage <cowmessage@gmail.com>
2017-09-01 15:41:31 +0300

    Update railtie.rb

--- lib/sprockets/commoner/railtie.rb
+++ lib/sprockets/commoner/railtie.rb
@@ -5,7 +5,6 @@ module Sprockets
         # We need to disable debugging because otherwise Rails will include each file individually, while we need everything to be bundled up together into a single file.
         # ^ Not applicable to sprockets 4+
         config.assets.debug = false unless Commoner.sprockets4?
-        config.assets.paths << 'node_modules'
       end
     end
   end
